4d3d5d89e66795b68ec87fa0d5ac7e490aa0c1bf
Mike L <Twirl1511@yandex.ru>
2021-04-24 12:49:19 +0300

    !F start

---
+++ BestGameEver/Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlatformMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            MoveDown(1f);
+        }
+    }
+
+    public void MoveDown(float mass)
+    {
+        float y = transform.position.y;
+        
+            transform.DOMoveY(y - mass, speed);
+    }
+}
