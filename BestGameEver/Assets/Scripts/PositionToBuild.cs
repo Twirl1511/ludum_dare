bc9e5ea45ca604272d709bf860123b8eb2a754de
Mike L <Twirl1511@yandex.ru>
2021-04-24 13:50:19 +0300

    !F можно строить

---
+++ BestGameEver/Assets/Scripts/PositionToBuild.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionToBuild : MonoBehaviour
+{
+    private bool _isOcupied;
+
+    public bool IsOcupied()
+    {
+        return _isOcupied;
+    }
+    public void SetIsOcupied(bool flag)
+    {
+        _isOcupied = flag;
+    }
+}
