00a5f43b619948ed7bf19024d96c3d21de8463e4
Mike L <Twirl1511@yandex.ru>
2021-04-25 20:32:05 +0300

    !F подсветка клеток

---
+++ BestGameEver/Assets/HiglightPositionsToBuild.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HiglightPositionsToBuild : MonoBehaviour
+{
+    private Color _positionColor;
+    private Color _positionColorHighlight;
+    private Color _positionColorDefault;
+    [SerializeField] private Renderer renderer;
+    private Vector3 _position;
+    void Start()
+    {
+        //GetComponent<SphereCollider>().radius = BuildingController.singleton._ropeLength;
+        _positionColor = renderer.material.color;
+        float r = _positionColor.r;
+        float g = _positionColor.g;
+        float b = _positionColor.b;
+        _positionColorHighlight = new Color(r, g, b, 0.4f);
+        _positionColorDefault = new Color(r, g, b, 0f);
+        _position = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PositionToBuild"))
+        {
+            other.GetComponent<Renderer>().material.SetColor("_Color", _positionColorHighlight);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PositionToBuild"))
+        {
+            other.GetComponent<Renderer>().material.SetColor("_Color", _positionColorDefault);
+        }
+    }
+
+
+
+    public void HiglightOff()
+    {
+        transform.DOMoveY(30, 2).From();
+        print(10);
+        StartCoroutine(SetAtciveFalse());
+    }
+
+
+    IEnumerator SetAtciveFalse()
+    {
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+        
+    }
+}
