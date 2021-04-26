cff705fae000b3a3a5046d5e9d8780044ae69009
Mike L <Twirl1511@yandex.ru>
2021-04-26 08:11:17 +0300

    !F камера ползет вниз, на камере скрипт, можно менять время и нижнюю точку

---
+++ BestGameEver/Assets/Scripts/CameraMoveDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraMoveDown : MonoBehaviour
+{
+    public float time;
+    public float deadLine;
+    void Start()
+    {
+        InvokeRepeating(nameof(MoveDown), time, time);
+    }
+
+    private void MoveDown()
+    {
+        if (transform.position.y >= deadLine - 1)
+        {
+            transform.DOMoveY(transform.position.y - 1, 5);
+        }
+        
+        
+    }
+}
